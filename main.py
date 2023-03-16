f659858c485df1180025d4c37bfa76afa1a18400
JT_1180 <126540229+JT1180@users.noreply.github.com>
2023-03-16 21:44:38 +0000

    Completed 3-2

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    email = input('Enter your email: ')
+  
+    if(email[0].isalpha()):
+      if(len(email)> 5 and len(email) <30):
+        if(email.find('@')):
+          if('.' in email):
+            print('True')
+          else:
+            print('False')
+        else:
+          print('False')
+      else:
+        print('False')
+    else:
+      print('False')
     pass
 
-    email = input('Enter your string ')
 
-    flag = True
-    if not email[0].isalpha():
-        flag = False
-    lenemail = len(email)
-    if lenemail <= 5 or lenemail >= 30:
-        flag = False
-    if email.find('@') == -1:
-        flag = False
-    else:
-        atidx = email.find('@')
-    if email[atidx + 1:].find('.') == -1:
-        flag = False
 
-    print(flag)
-    
-      
+  
 if __name__ == '__main__':
     main()
